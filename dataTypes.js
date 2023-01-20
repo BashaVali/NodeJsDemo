130e0337d499f2a03a2cee3e81112270974731f1
BashaVali <bashavali222@gmail.com>
2023-01-21 04:45:21 +0530

    Demo DataTypes

---
+++ dataTypes.js
@@ -0,0 +1,21 @@
+var val; val ="foo"
+let datatype1=typeof val; datatype1="undefined"
+var val; val ="foo"
+let datatype2=typeof val; datatype2="number"
+var val; val ="foo"
+let datatype3=typeof val; datatype3="bigint"
+var val; val ="foo"
+let datatype4=typeof val; datatype4="boolean"
+var val; val ="foo"
+let datatype5=typeof val; datatype5="string"
+var val; val ="foo"
+let datatype6=typeof Symbol("id"); datatype6="symbol"
+var val; val ="foo"
+let datatype7=typeof Math; datatype7="object"
+var val; val ="foo"
+let datatype8=typeof nullval; datatype8="object"
+let sayHi =function(){ sayHi =f()
+     console.log.apply("Say Hi");
+    };
+    let datatype9 =typeof sayHi;
+    console.log(datatype9);
