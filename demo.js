b10c2ec795ca4ad09c1e8fd80b00369aba8492ad
BashaVali <bashavali222@gmail.com>
2023-01-21 04:08:57 +0530

    Demo

---
+++ demo.js
@@ -0,0 +1,15 @@
+let a=10;
+let b=20;
+let c=a+b;
+console.log("Total value:-"+c);
+
+
+let s1="Basha";
+let s2="Vali";
+let s=s1+s2;
+console.log("String concat "+s);
+
+let d=new Date();
+let today ="month is :-"+d.getMonth()+1+"date :-d.getDate()+year :-"+d.getFullYear();
+let time=d.getTime();
+console.log(today);
